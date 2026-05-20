1dbce4cd02b84325a1c5659e9c4b3500b6017bbb
iurii Chmykhun <ichmykhun@omnitech-inc.com>
2026-05-20 08:42:41 -0500

    centered grid & beautiful

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const gameGrid = document.getElementById("game-grid")
 
-class Player {
+var gridSize = 9
+class GameState {
     /* color, id, position, etc. */
 }
 
+// when page loads
 document.addEventListener("DOMContentLoaded", (event) => {
     // create 9x9 grid of divs
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < gridSize; i++) {
         let row = document.createElement("div")
         row.id = "row-" + i.toString()
 
         gameGrid.appendChild(row)
 
-        for (let j = 0; j < 9; j++) {
+        for (let j = 0; j < gridSize; j++) {
             let col = document.createElement("div")
             col.id = "col-" + j.toString()
             col.textContent="";
